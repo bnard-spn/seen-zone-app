9e2632ba17560d709001a73ad9c490d82f0e1a9f
Bernardino Silvestre jr <Bernardino Silvestre jr>
2017-04-23 19:16:30 +0800

    Created an initial landing page and front-end design

---
+++ application/views/pages/home.php
@@ -0,0 +1,16 @@
+<div class="row">
+	<div class="col-md-6">
+		<h3>Welcome to the SEEN-Zone Application</h3>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-2">
+		<div class="list-group table-of-contents">
+			<a class="list-group-item" href="#">Home</a>
+			<a class="list-group-item" href="#">Profile</a>
+			<a class="list-group-item" href="#">Groups</a>
+			<a class="list-group-item" href="#">Schedule</a>
+			<a class="list-group-item" href="#">Grades</a>
+		</div>
+	</div>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Seen-Zone</title>
+		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/solar/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?php echo base_url(); ?>">SEEN-Zone</a>
+				</div>
+				<div id="navbar">
+					<ul class="nav navbar-nav">
+						<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="container">
